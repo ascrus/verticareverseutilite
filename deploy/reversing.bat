86200a25321a7b5bb37e42f2e472f107d6080796
Алексей Константинов <ascrus@yandex.ru>
2018-03-01 19:46:07 +0300

    Added a deployment directory for saving the last stable release. Switching GETL to version 2.0.2. The build.gradle is rewritten.

---
+++ deploy/reversing.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set IZPACK_JAVA="%JAVA_HOME%"
+ 
+if exist "%IZPACK_JAVA%\bin\javaw.exe" (
+  set LOCAL_JAVA=%IZPACK_JAVA%\bin\javaw.exe
+) else (
+  set LOCAL_JAVA=javaw.exe
+)
+
+echo Using java: %LOCAL_JAVA%
+
+start "SQLReversingForVertica" /B "%LOCAL_JAVA%" -splash:splash.gif -jar SQLReversingForVertica-0.0.6-SNAPSHOT.jar
---
+++ deploy/reversing.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+IZPACK_JAVA_HOME=%JAVA_HOME
+
+if [ -d "$JAVA_HOME" -a -x "$JAVA_HOME/bin/java" ]; then
+	JAVACMD="$JAVA_HOME/bin/java"
+elif [ -d "$IZPACK_JAVA_HOME" -a -x "$IZPACK_JAVA_HOME/bin/java" ]; then
+	JAVACMD="$IZPACK_JAVA_HOME/bin/java"
+else
+	JAVACMD=java
+fi
+
+$JAVACMD -splash:splash.gif -jar SQLReversingForVertica-0.0.5-SNAPSHOT.jar
